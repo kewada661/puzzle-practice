2ffcbc3c8fa1f2e012855f4d34a05bb8accde32e
kewada661 <kewada661@gmail.com>
2026-09-10 17:17:43 -0700

    Refine Login behavior
    Refactor loading in Auth component to fit react router idiom

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,6 +2,22 @@ import type { Route } from "../../.react-router/types/src/routes/+types/layout";
 import { Header } from "../components";
 import { Outlet } from "react-router";
 import { AuthContextProvider } from "../context";
+import { useAuth } from "../hooks";
+
+export const clientLoader = async () => {
+  const {
+    refreshSession
+  } = useAuth();
+  try {
+    await refreshSession();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export const hydrateFallback = () => {
+  return <div>LOADING...</div>
+}
 
 const Layout = ({ matches }: Route.ComponentProps) => {
   const showHeader = matches[2] && matches[2].pathname !== "/";
